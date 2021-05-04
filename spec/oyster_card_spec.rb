68050a426f2add6ac875ca6dc47f65825dd9e718
Charlie Slater <charliemslater1701@gmail.com>
2021-05-04 15:19:42 +0100

    add top_up

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,9 +1,16 @@
-require './lib/oystercard.rb'
+require './lib/oyster_card.rb'
 
 describe OysterCard do
-  describe '#balance' do
+  describe '.new' do
     it 'must have a default balance of 0' do
       expect(subject.balance).to eq(0)
+    end
+  end
+
+  describe '#top_up' do
+    it 'adds to the balance' do
+      subject.top_up(50)
+      expect(subject.balance).to eq(50)
     end
   end
 end
